3f6067ba0db900aef5d32c9b8706d144b7acc235
BetulBNY <betulbny@gmail.com>
2026-05-21 13:39:44 +0300

    Cleaned data fetched from postgresql to pandas df. docker compose ingest service changed to analysis_app.

---
+++ src/fetch_clean_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+# 1. Bağlantı Bilgilerim (Docker compose'daki bilgilerle aynı)
+user = os.getenv('POSTGRES_USER')
+password = os.getenv('POSTGRES_PASSWORD')
+db_name = os.getenv('POSTGRES_DB')
+
+host = 'postgres'
+port = "5432"
+
+# 2. Connection String ve Engine Oluştur
+conn_string = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
+engine = create_engine(conn_string)
+
+# 3. SQL sorgusunu yazarak temilenmiş veriyi pandas DataFrame'ine çekelim.
+print("Veriler PostgreSQL'den çekiliyor...")
+query = "SELECT * FROM cleaned_retail_data"
+df = pd.read_sql(query, engine)
+
+print(f"Başarılı! {df.shape[0]} satır ve {df.shape[1]} sütun veri yüklendi.")
+
+# 4. İlk Kontrol
+print(df.head())
+
+# 5. data klasörünün içine temizlenmiş veriyi kaydetme
+df.to_csv("data/cleaned_retail_data.csv", index=False)
+
+# docker compose exec analysis_app python fetch_clean_data.py
