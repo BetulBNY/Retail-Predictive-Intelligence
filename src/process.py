30300920efea1711805f76cee494d22473def9fe
BetulBNY <betulbny@gmail.com>
2026-05-21 19:41:00 +0300

    Winsorization and Logaritmic transformation applied

--- src/process.py
+++ src/process.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("data/cleaned_retail_data.csv")
 
@@ -45,6 +46,53 @@ monetary     5878.0   2869.961142  14002.181211     2.95  330.6825   835.97  218
 """
 # -------------------------------- Outlier Handling --------------------------------
 # IQR yöntemi ile outlierleri tespit edip onlara threshold uygulayarak outlier'ları sınırlandıracağım.
+
+# Winsorization / Cap: Outlier'ları belirli bir eşik değere (threshold) göre sınırlandırma tekniği. 
+# Trimming: Outlier'ları tamamen veri setinden çıkarma tekniği. 
+
+def outlier_thresholds (data_frame,column, q1=0.10, q3=0.90):
+    quartile1 = data_frame[column].quantile(q1)
+    quartile3 = data_frame[column].quantile(q3)
+    IQR = quartile3 - quartile1
+    min_threshold = quartile1 - 1.5 * IQR
+    max_threshold = quartile3 + 1.5 * IQR
+    return min_threshold, max_threshold
+
+def replace_with_thresholds(data_frame, column):
+    min_threshold, max_threshold = outlier_thresholds (data_frame, column)
+    data_frame.loc[(data_frame[column] < min_threshold), column] = min_threshold
+    data_frame.loc[(data_frame[column] > max_threshold), column] = max_threshold
+    
+def check_outlier(data_frame, column):
+    min_threshold, max_threshold = outlier_thresholds (data_frame, column)
+    if data_frame[(data_frame[column] < min_threshold) | (data_frame[column] > max_threshold)].any(axis=None):
+        return True
+    else:
+        return False
+    
+for col in ["recency", "frequency", "monetary"]:
+    if check_outlier(rfm, col):
+        print(f"{col} için outlier var. Threshold'lar uygulanıyor...")
+        # outlier olanları bulma:
+        min_threshold, max_threshold = outlier_thresholds (rfm, col)
+        outlier_indices = rfm[(rfm[col] < min_threshold) | (rfm[col] > max_threshold)].index
+        outlier_values = rfm.loc[outlier_indices, col]
+        print(f"Outlier değerler:\n{outlier_values}\n")
+        # outlier'ları threshold'lara göre sınırlandırma:
+        replace_with_thresholds(rfm, col)
+
+print("Outlier'lar sınırlandırıldıktan sonra RFM değerlerinin istatistikleri:")
+print(rfm.describe([0.25, 0.5, 0.75, 0.97]).T)
+
+# -------------------------------- Logarithmic Transformation --------------------------------
+# RFM değerlerinden F ve M sağa çarpık dağılım gösteryor. Logaritmik dönüşüm, bu tür dağılımları normalize etmek için kullanıldı.
+rfm["frequency"] = np.log1p(rfm["frequency"]) # log(0) hatasından kurtulmak için log yerine log1p kullandım (log(1+x))
+rfm["monetary"] = np.log1p(rfm["monetary"]) # log(0) hatasından kurtulmak için log yerine log1p kullandım (log(1+x))
+
+print("Logaritmik dönüşüm sonrası RFM değerlerinin istatistikleri:")
+print(rfm.describe([0.25, 0.5, 0.75, 0.97]).T)
+
+# -------------------------------- Scaling --------------------------------
 
 # -------------------------------- Base RFM K-Means  --------------------------------
 
